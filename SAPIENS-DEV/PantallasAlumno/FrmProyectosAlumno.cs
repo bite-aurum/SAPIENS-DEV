9de74e85f9f31c743fdb4ffb983373da756de8e1
bite-aurum <saidmiguelmezalopez@gmail.com>
2026-07-20 19:24:07 -0600

    Soporte para rol de alumno y nuevos formularios UI
    Se añadió el soporte para el rol "alumno" en el sistema. Al iniciar sesión como alumno, se abre el formulario FrmMenuAlumno con navegación y dashboard específicos. Se agregaron los formularios y diseñadores para FrmMenuAlumno, FrmDashboardAlumno, FrmProyectosAlumno y FrmDetalleProyectoAlumno. El dashboard muestra estadísticas, proyectos, subtareas y notificaciones. Se implementó la navegación lateral y encabezado para alumno. Se actualizaron los archivos .csproj y se ajustó el flujo de login para el nuevo rol.

---
+++ SAPIENS-DEV/PantallasAlumno/FrmProyectosAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAPIENS_DEV.PantallasAlumno
+{
+	public partial class FrmProyectosAlumno : Form
+	{
+		public FrmProyectosAlumno()
+		{
+			InitializeComponent();
+		}
+	}
+}
